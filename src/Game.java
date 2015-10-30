d55c1389c4707056bf4ac57d81ad0f3496faad19
Trent <trweatherman1@catamount.wcu.edu>
2015-10-30 15:58:08 -0400

    game with sos board

---
+++ src/Game.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+/**
+ * Models a game of SOS
+ * @author Trent Weatherman
+ * @author Daniel Powell
+ * @version 2015
+ */
+
+public class Game {
+    /** Current player. From 0 to n */
+    private int                 player;
+
+    /** Hold the scores of all the players */
+    private int[]               scores;
+
+    /** The sos board */
+    private SosBoard            board;
+
+    /** For input */
+    private Scanner             scanIn;
+
+    /** Current allowed number of players */
+    private static final int    PLAYERS = 2;
+
+    /**
+     * Initialize everything for a new game of SOS. Player 0 always goes first.
+     */
+    public Game() {
+        player = 0;
+        scores = new int[PLAYERS];
+        board  = new SosBoard(3);   // TODO: allow user to enter this via keyboard
+        scanIn = new Scanner(System.in);
+    }
+
+    /**
+     * The method where gameplay takes place until the board is full.
+     */
+    public void go() {
+
+        do {
+            System.out.println(board);
+            makeMove();
+            changePlayer();
+            displayScore();
+        } while(!board.isFull());
+
+
+        // QUESTION: What is the problem with the how the winner is determined?
+        int highScore = 0;
+        for (int i = 1; i < PLAYERS; i++) {
+            if (scores[i] > scores[highScore])
+                highScore = i;
+        }
+
+        System.out.println("The winner is: Player " + highScore);
+
+    }
+
+    /**
+     * Print the current score of all players to standard out.
+     */
+    private void displayScore() {
+        for (int current = 0; current < PLAYERS; current++) {
+            System.out.println("Player " + current + " has a score of " + scores[current]);
+        }
+    }
+
+    /**
+     * Determines whos turn it is 
+     */
+    private void changePlayer() {
+        // QUESTION: What in the world is going on here?
+        player = (player + 1) % PLAYERS;
+    }
+
+    /**
+     * Allow a player to make a valid move.
+     */
+    private void makeMove() {
+        boolean done = false;
+        int row = -1;
+        int col = -1;
+        String msg = "";
+        do {
+            System.out.print(msg);
+            System.out.print("Player " + player + " choose a row and column >");
+            row = scanIn.nextInt();
+            col = scanIn.nextInt();
+
+            System.out.print("\nWould you like to play 'S' or 'O' >");
+            char icon = scanIn.next().toUpperCase().charAt(0);
+            done = board.setSpot(row, col, icon);
+
+            msg = "Invalid move, try again\n";
+        } while(!done);
+
+        // If we get here the player made a valid move and we see if the got any points. 
+        scores[player] += board.calculatePoints(row, col);
+    }
+
+    /**
+     * Entry point into this program.
+     * @param args Not used.
+     */
+    public static void main(String[] args) {
+        Game driver = new Game();
+        driver.go();
+    }
+
+}
---
+++ src/SosBoard.java
@@ -0,0 +1,203 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * Models and SOS board
+ * @author Trent Weatherman 
+ * @author Daniel Powell
+ * @version 2015
+ */
+
+public class SosBoard {
+
+    // TODO: Make fields
+    private char board[][];
+    private int  empty;
+
+    // TODO: Finish this method
+    public SosBoard(int size) {
+        board = new char [size][size];
+        initBoard();
+    }
+
+    // TODO: Finish this method
+    private void initBoard() {
+        // WRITE ME
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board.length; j++)
+            {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    // TODO: Given
+    public boolean setSpot(int row, int col, char icon) {
+        boolean result = false;
+        icon = Character.toUpperCase(icon);
+        if ((icon == 'S' || icon == 'O' ) &&
+                row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            if (board[row][col] == ' ') {
+                board[row][col] = icon;
+                result = true;
+                empty++;
+            }
+        }
+        return result;
+    }
+
+    // We are making the assumption that this array has the same number of rows and columns.
+    // TODO: Finish this method
+    public boolean isFull() {
+
+        int count = 0;
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board.length; j++)
+            {
+                if(board[i][j] == (' '))
+                {
+                    count++;
+                }
+            }
+
+        }
+
+        if(count > 0)
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    // TODO: Given
+    public int calculatePoints(int row, int col) {
+        return checkCompass(row, col) + checkDiag(row, col);
+    }
+
+    // TODO: Finish this method
+    public boolean inBounds(int num) {
+        if(num >= 0 && num < board.length)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // TODO: Finish this method
+    public int checkCompass(int row, int col) {
+        int points = 0;
+
+
+        // YOUR CODE GOES HERE
+        if(board[row][col] == 'O' || board[row][col] == 'o')
+        {
+            if(inBounds(row+1) && inBounds(row - 1))
+            {
+                if(board[row + 1][col] == 'S' && board[row - 1][col] == 'S')
+                {
+                    points++;
+                }
+            }
+            if(inBounds(col + 1) && inBounds(col - 1))
+            {
+                if(board[row][col - 1] == 'S' && board[row][col + 1] == 'S')
+                {
+                    points++;
+                }
+            }
+        }
+        if(board[row][col] == 'S' || board[row][col] == 's')
+        {
+
+            if(inBounds(row + 2))
+            {
+                if(board[row + 1][col] == 'O' && board[row +  2][col] == 'S')
+                {
+                    points++;
+                }
+            }
+            if(inBounds(row - 2))
+            {
+                if(board[row - 1][col] == 'O' && board[row -  2][col] == 'S')
+                {
+                    points++;
+                }
+
+            }
+            if(inBounds(col + 2))
+            {
+                if(board[row][col + 1] == 'O' && board[row][col + 2] == 'S')
+                {
+                    points++;
+                }
+            }
+            if(inBounds(col - 2))
+            {
+                if(board[row][col - 1] == 'O' && board[row][col - 2] == 'S')
+                {
+                    points++;
+                }
+            }
+
+        }
+        return points;
+    }
+
+    // TODO: You do not have to complete this
+    public int checkDiag(int row, int col) {
+        int points = 0;
+        return points;
+    }
+
+/*##########################################################################################
+  # Do not modify under this divider                                                       #
+  ##########################################################################################*/
+
+    public String toString() {
+        StringBuilder result = new StringBuilder(getEdge());
+        result.append(getDivider());
+        for (int row = 0; row < board.length; row++) {
+            result.append(row + " |");
+            for (int col = 0; col < board[row].length; col++) {
+                result.append(" " + board[row][col] + " +");
+            }
+            // This line is kind of a hack. #sorrynotsorry
+            result.append("\b|\n" + getDivider());
+        }
+        return result.toString();
+    }
+
+    /**
+     * Prints out the first and/or last line in the sos board.
+     * @return the first/last line of a matrix.
+     */
+    private String getDivider() {
+        StringBuilder result = new StringBuilder("  +");
+        for (int row = 0; row < board.length; row++) {
+            result.append("---+");
+        }
+        result.append("\n");
+        return result.toString();
+    }
+
+    /**
+     * Prints out divider lines between playing spaces.
+     * @return divider lines between rows of values.
+     */
+    private String getEdge() {
+        StringBuilder result = new StringBuilder("   ");
+        for (int row = 0; row < board.length; row++) {
+            result.append(" " + row + "  ");
+        }
+        result.append("\n");
+        return result.toString();
+    }
+}
